456add78e0da0e0ac3c1f6734dadb8078c0cf5eb
Vadzim Miliantsei <Vadzim.Miliantsei@gmail.com>
2020-10-18 14:35:29 +0300

    new package: tcplay-veracrypt

---
+++ packages/tcplay-veracrypt/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/veracrypt/tc-play
+TERMUX_PKG_DESCRIPTION="Free and simple TrueCrypt implementation based on dm-crypt."
+TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_SRCURL=https://github.com/veracrypt/tc-play.git
+TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_VERSION=2.0
+TERMUX_PKG_DEPENDS="libdevmapper, libuuid, libgcrypt"
